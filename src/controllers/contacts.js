0b8a8be8c9b00f5d42de3a7bb834e8efc44419c1
AlexNovickiy <novickiisasha78@gmail.com>
2025-09-05 15:28:52 +0300

    first improvements

---
+++ src/controllers/contacts.js
@@ -0,0 +1,32 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    throw createHttpError(400, 'Invalid contact ID format');
+  }
+
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, `Contact with id ${contactId} not found`);
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
