3690e4395676152221481130c5754feab327f789
AlexNovickiy <novickiisasha78@gmail.com>
2025-09-06 14:07:14 +0300

    feat: implement CRUD operations for contacts and enhance not found error handling

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,10 @@
+import createHttpError from 'http-errors';
+
 export const notFoundHandler = (req, res, next) => {
-  res.status(404).json({
-    message: 'Route not found',
+  const err = createHttpError(404, 'Route not found');
+  res.status(err.status).json({
+    status: err.status,
+    message: err.name,
+    data: err,
   });
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,15 +2,37 @@ import { Router } from 'express';
 import {
   getContactsController,
   getContactByIdController,
+  createContactController,
+  deleteContactController,
+  upsertContactController,
+  patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
+
 contactsRouter.get(
   '/contacts/:contactId',
   ctrlWrapper(getContactByIdController),
+);
+
+contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+
+contactsRouter.delete(
+  '/contacts/:contactId',
+  ctrlWrapper(deleteContactController),
+);
+
+contactsRouter.put(
+  '/contacts/:contactId',
+  ctrlWrapper(upsertContactController),
+);
+
+contactsRouter.patch(
+  '/contacts/:contactId',
+  ctrlWrapper(patchContactController),
 );
 
 export default contactsRouter;
